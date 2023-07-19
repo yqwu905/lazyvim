9757147998783905528479f922efaf4a0c7193f4
wuyuanqing <wuyuanqing2@huawei.com>
2023-07-19 09:59:29 +0800

    feat: Remove yanky

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,7 +12,6 @@ require("lazy").setup({
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.lang.clangd" },
-        { import = "lazyvim.plugins.extras.coding.yanky" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
         { import = "plugins" },
