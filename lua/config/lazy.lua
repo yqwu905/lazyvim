1f6b343a95031f84da2baf70c9c09ef1dba305f5
wuyuanqing <wuyuanqing2@huawei.com>
2023-07-19 16:46:14 +0800

    feat: Add layers for language
    Add json, pyhton and ruby layers

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,9 @@ require("lazy").setup({
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.lang.clangd" },
+        { import = "lazyvim.plugins.extras.lang.json" },
+        { import = "lazyvim.plugins.extras.lang.python" },
+        { import = "lazyvim.plugins.extras.lang.ruby" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         -- import/override with your plugins
         { import = "plugins" },
