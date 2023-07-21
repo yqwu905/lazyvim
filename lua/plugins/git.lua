5e4ab0542b12060574fc52efde15eeec31715c71
wuyuanqing <wuyuanqing2@huawei.com>
2023-07-21 18:15:54 +0800

    feat: Add plugins
    - project.nvim
    - diffview.nvim

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,4 +3,17 @@ return {
         "f-person/git-blame.nvim",
         event = { "BufReadPre", "BufNewFile" },
     },
+
+    {
+        "sindrets/diffview.nvim",
+        cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory", "DiffviewRefresh" },
+        keys = {
+            { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "diffview close" },
+            { "<leader>gdf", "<cmd>DiffviewFileHistory %<cr>", desc = "diffview current file" },
+            { "<leader>gdh", "<cmd>DiffviewFileHistory<cr>", desc = "diffview history" },
+            { "<leader>gdv", "<cmd>DiffviewOpen<cr>", desc = "diffview" },
+        },
+        opts = {},
+        lazy = true,
+    },
 }
